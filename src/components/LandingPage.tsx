c49cb18d4d179ae4038c583d9826d2b6322924cd
Rakesh Sharma <rakeshsharmabgp29@gmail.com>
2023-06-28 11:19:16 +0530

    landing Page

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,7 +1,42 @@
 import React from "react";
+import { FiSearch } from "react-icons/fi";
 
 const LandingPage = () => {
-  return <div>LandingPage</div>;
+  return (
+    <div className="w-full bg-[#FDF8EE] border-b border-primary border-slate-600">
+      <div className="flex flex-col md:flex-row">
+        <div className="w-full mt-20 mb-20 ml-24 md:w-1/2 order-2 md:order-1">
+          <div className="text-center md:text-left">
+            <h2 className="text-3xl font-bold mb-4">The
+              <span className="text-orange"> Smart</span>
+            </h2>
+            <h2 className="text-3xl font-bold mb-4">Choice For
+              <span className="text-orange"> Future</span>
+            </h2>
+            <h2 className="text-xl text-slate-500 mb-4">
+              Elearn is a global training provider based across the UK that
+              specialises in accredited and bespoke training courses. We crush
+              the...
+            </h2>
+          </div>
+          <div className="flex items-center mt-4">
+            <div className="flex items-center mr-2">
+              <FiSearch className="h-6 w-6 text-gray-600" />
+            </div>
+            <input
+              type="text"
+              placeholder="Search for a location..."
+              className="border border-gray-300 px-4 py-2 rounded-full flex-grow"
+            />
+            <button className="primary-btn">Continue</button>
+          </div>
+        </div>
+        <div className="w-full mt-20 mb-20 mr-24 md:w-1/2 order-1 md:order-2">
+          <img src="landing.png" alt="Image" className="w-full" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LandingPage;
